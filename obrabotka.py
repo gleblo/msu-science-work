7730ecc6f6921f06c7839484bb8a4ddaf3aa03d7
N <gleb.nuzhdov@gmail.com>
2020-12-16 22:35:24 +0300

    debug

--- obrabotka.py
+++ obrabotka.py
@@ -6,19 +6,19 @@
 
 def cut_long_tails(im, lbl):
 
-	# images = []
-	labels = []
-	# indcs = [ i for i in range(len(im)) if len(im[i])<26  and len(im[i])>22 and len(im[i][0]) < 65 and len(im[i][0]) > 60 ]
-	# for i in indcs:
-	#     img = im[i].copy()
+    # images = []
+    labels = []
+    # indcs = [ i for i in range(len(im)) if len(im[i])<26  and len(im[i])>22 and len(im[i][0]) < 65 and len(im[i][0]) > 60 ]
+    # for i in indcs:
+    #     img = im[i].copy()
 
-	#     # img = cv2.resize(img, dsize=(128, 32), interpolation=cv2.INTER_CUBIC)
-	#     img = np.expand_dims(img , axis = 2)
+    #     # img = cv2.resize(img, dsize=(128, 32), interpolation=cv2.INTER_CUBIC)
+    #     img = np.expand_dims(img , axis = 2)
 
-	#     # Normalize each image
-	#     img = img/255.
+    #     # Normalize each image
+    #     img = img/255.
 
-	#     images.append(img)
-	#     labels.append(lbl[i])
+    #     images.append(img)
+    #     labels.append(lbl[i])
 
-	return im, lbl
+    return im, lbl
