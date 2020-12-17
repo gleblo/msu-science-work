2074a4c953a6f63f62e54d474aa32a1f696bb48e
N <gleb.nuzhdov@gmail.com>
2020-12-17 17:39:59 +0300

    debug

--- obrabotka.py
+++ obrabotka.py
@@ -25,7 +25,7 @@ def remove_shit(im, lbl):
 		img = im[i].copy()
 		label = lbl[i]
 
-		if len(img)<26  or len(img)>22 or len(img[0]) < 65 or len(img[0]) > 60 or consists_of_shit(label):
+		if len(img)<26 and len(img)>22 and len(img[0]) < 65 and len(img[0]) > 60 and (not consists_of_shit(label)):
 			continue
 
 		img = cv2.resize(img, dsize=(128, 32), interpolation=cv2.INTER_CUBIC)
