4dc4eed91eda5157e8dff7805711f0b19b12ba38
N <gleb.nuzhdov@gmail.com>
2020-12-17 17:32:07 +0300

    Remove shit

--- obrabotka.py
+++ obrabotka.py
@@ -3,22 +3,39 @@ import numpy as np
 
 
 
+def consists_of_shit(num):
+    shit = {"0","1","-","."}
+    set_digs = set(str(num))
+    if (set_digs <= shit):
+        return True
+    else:
+        return False
 
-def cut_long_tails(im, lbl):
+
+assert consists_of_shit(-0.1) == True
+assert (consists_of_shit(0.2) == False)
+
+
+
+def remove_shit(im, lbl):
 
 	images = []
 	labels = []
-	indcs = [ i for i in range(len(im)) if len(im[i])<26  and len(im[i])>22 and len(im[i][0]) < 65 and len(im[i][0]) > 60 ]
 	for i in indcs:
 	    img = im[i].copy()
+        label = lbl[i]
+
+        if len(img)<26  or len(img)>22 or len(img[0]) < 65 or len(img[0]) > 60 or consists_of_shit(label)
+            continue
 
 	    img = cv2.resize(img, dsize=(128, 32), interpolation=cv2.INTER_CUBIC)
 	    img = np.expand_dims(img , axis = 2)
-
-	    # Normalize each image
 	    img = img/255.
 
+
 	    images.append(img)
-	    labels.append(lbl[i])
+	    labels.append(label)
 
 	return images, labels
+
+
