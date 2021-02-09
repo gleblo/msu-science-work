b9a1248e64fc0fce54cd1aea88ed698d0e36f4ba
gleblo <32290989+gleblo@users.noreply.github.com>
2021-02-10 00:47:08 +0300

    Пытаемся закинуть всё в облаке

--- obrabotka.py
+++ obrabotka.py
@@ -39,3 +39,55 @@ def remove_shit(im, lbl):
 	return images, labels
 
 
+
+
+def to_char(num):
+    return ("%.1f" % (num))
+
+
+def remove_dot(txt):
+    return txt[:-2] + txt[-1:]
+
+
+
+def reparo(lbl):
+    return [remove_dot(to_char(lb)) for lb in lbl]
+
+
+
+def max_len(lbl):
+    return max([len(lb) for lb in lbl])
+
+
+def encode_to_labels(txt):
+    # encoding each output word into digits
+    dig_lst = []
+    for index, char in enumerate(txt):
+        try:
+            dig_lst.append(char_list.index(char))
+        except:
+            print(char)
+        
+    return dig_lst
+
+def get_enumed(lbl):
+    return [encode_to_labels(lb) for lb in lbl]
+
+
+
+# def get_max_len(lbl)
+ 
+
+
+
+def img_prep(imd):
+    img = cv2.resize(imd, dsize=(128, 32), interpolation=cv2.INTER_CUBIC)
+    img = np.expand_dims(img , axis = 2)
+
+    # Normalize each image
+    img = img/255.
+    return img
+
+def imgs_prep(im):
+    return [img_prep(img) for img in im]
+
